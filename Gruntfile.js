ac33320a4acfca5a1060e31e9ea0c2276a677658
Damir Brekalo <dbrekalo@gmail.com>
2015-12-28 13:08:59 +0100

    - demo and documentation update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,101 +1,125 @@
 /* jshint node: true */
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		uglify: {
-			min: {
-				files: [{
-					expand: true,
-					cwd: 'src',
-					src: '**/*.js',
-					dest: 'dist',
-					ext: '.min.js'
-				}],
-				options: {
+        pkg: grunt.file.readJSON('package.json'),
 
-				}
-			}
-		},
+        uglify: {
+            min: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: '**/*.js',
+                    dest: 'dist',
+                    ext: '.min.js'
+                }],
+                options: {
 
-		copy: {
-			jsFiles: {
-				files: [{
-					expand: true,
-					cwd: 'src',
-					src: ['**/*.js'],
-					dest: 'dist'
-				}]
-			}
-		},
+                }
+            }
+        },
 
-		jshint: {
-			options: {
-				'jshintrc': '.jshintrc'
-			},
-			all: ['src','Gruntfile.js']
-		},
+        copy: {
+            jsFiles: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['**/*.js'],
+                    dest: 'dist'
+                }]
+            }
+        },
 
-		jscs: {
-		    options: {
-		        config: '.jscsrc'
-		    },
-		    scripts: {
-		        files: {
-		            src: [
-		                'src/**/*.js'
-		            ]
-		        }
-		    }
-		},
+        jshint: {
+            options: {
+                'jshintrc': '.jshintrc'
+            },
+            all: ['src','Gruntfile.js']
+        },
 
-		sass: {
-			min: {
-				files: {
-					'dist/simpleLightbox.min.css': 'src/simpleLightbox.scss'
-				},
-				options: {
-				    outputStyle: 'compressed',
-				    sourceMap: false,
-				    precision: 5
-				}
-			},
-			expanded: {
-				files: {
-					'dist/simpleLightbox.css': 'src/simpleLightbox.scss'
-				},
-				options: {
-				    outputStyle: 'expanded',
-				    sourceMap: false,
-				    precision: 5
-				}
-			}
-		},
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            scripts: {
+                files: {
+                    src: [
+                        'src/**/*.js'
+                    ]
+                }
+            }
+        },
 
-		watch: {
-			jsFiles: {
-				expand: true,
-				files: ['src/**/*.js'],
-				tasks: ['jshint', 'jscs', 'copy','uglify'],
-				options: {
-					spawn: false
-				}
-			},
-			cssFiles: {
-				expand: true,
-				files: ['src/**/*.scss'],
-				tasks: ['sass'],
-				options: {
-					spawn: false
-				}
-			}
-		}
+        sass: {
+            min: {
+                files: {
+                    'dist/simpleLightbox.min.css': 'src/simpleLightbox.scss'
+                },
+                options: {
+                    outputStyle: 'compressed',
+                    sourceMap: false,
+                    precision: 5
+                }
+            },
+            expanded: {
+                files: {
+                    'dist/simpleLightbox.css': 'src/simpleLightbox.scss'
+                },
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: false,
+                    precision: 5
+                }
+            }
+        },
 
-	});
+        includereplace: {
+            dist: {
+                options: {
+                    globals: {
+                        repositoryUrl: '<%= pkg.repository.url %>',
+                    },
+                    prefix: '{{ ',
+                    suffix: ' }}'
+                },
+                src: 'demo/index.html',
+                dest: 'index.html'
+            }
+        },
 
-	require('load-grunt-tasks')(grunt);
+        watch: {
+            jsFiles: {
+                expand: true,
+                files: ['src/**/*.js'],
+                tasks: ['jshint', 'jscs', 'copy','uglify'],
+                options: {
+                    spawn: false
+                }
+            },
+            cssFiles: {
+                expand: true,
+                files: ['src/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            },
+            demoFiles: {
+                expand: true,
+                files: ['demo/**/*.html'],
+                tasks: ['includereplace'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
 
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('build', ['jshint', 'jscs', 'uglify', 'copy', 'sass']);
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['jshint', 'jscs', 'uglify', 'copy', 'sass', 'includereplace']);
 
 };
